9fe1cc985c7fad43e450fd640c47d9c7352a8865
swaraj-jain <happy2000jain@gmail.com>
2023-03-09 22:31:16 +0530

    file structure modified

--- src/index.js
+++ src/index.js
@@ -10,15 +10,9 @@ mongoose.set('useCreateIndex', true);
 mongoose.set('useFindAndModify', false);
 
 const url = process.env.DATABASE_URL || "mongodb://0.0.0.0:27017/users";
-try{
-    mongoose.connect(url,{ useNewUrlParser: true, useUnifiedTopology: true }, () => {
-        console.log('connected to DB');
-    })
-}catch(err){
-    console.error(err);
-}
-
+mongoose.connect(url,{ useNewUrlParser: true, useUnifiedTopology: true }, () => {
+    console.log('connected to DB')
+})
 
 
 app.listen(3000, () => console.log('Server running......'));
-
